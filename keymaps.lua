0fef4d76ab929c947768f77879a3d38b72ed586b
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-09-09 18:31:00 +0100

    Refactor config into multiple files

---
+++ keymaps.lua
@@ -0,0 +1,46 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Vertically center current line when scrolling text with <C-u> and <C-d>
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Go up half a page and center' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Go down half a page and center' })
+
+-- Vertically center next/previous search occurrences
+vim.keymap.set('n', 'n', 'nzz', { desc = 'Jump to the next occurrence and center' })
+vim.keymap.set('n', 'N', 'Nzz', { desc = 'Jump to the previous occurrence and center' })
+
+-- Toggle nvim-tree
+vim.keymap.set('n', '`', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
