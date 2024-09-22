517c27872dbd208e8945d0bacd81c029fc76fc45
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-09-22 18:15:20 +0100

    Fix neo-tree not replacing netrw

--- lua/kickstart/plugins/ui/fs_explorer.lua
+++ lua/kickstart/plugins/ui/fs_explorer.lua
@@ -11,15 +11,30 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '`', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '`', ':Neotree toggle reveal_force_cwd<CR>', desc = 'NeoTree reveal', silent = true },
   },
+  init = function()
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = vim.api.nvim_create_augroup('Neotree_start_directory', { clear = true }),
+      desc = 'Start Neo-tree with directory',
+      once = true,
+      callback = function()
+        if package.loaded['neo-tree'] then
+          return
+        else
+          local stats = vim.uv.fs_stat(vim.fn.argv(0))
+          if stats and stats.type == 'directory' then
+            require 'neo-tree'
+          end
+        end
+      end,
+    })
+  end,
   opts = {
     filesystem = {
-      window = {
-        mappings = {
-          ['`'] = 'close_window',
-        },
-      },
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
       filtered_items = {
         hide_dotfiles = false,
         hide_gitignored = false,
